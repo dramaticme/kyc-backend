837dbe11c195da1ba6e61ae300343ad27e12fb34
Sashi Sonkusare <sonkusaresakshibhojraj@gmail.com>
2025-09-03 12:16:52 +0530

    Backend Setup

---
+++ src/main/java/com/kycapp/kyc_backend/KycBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kycapp.kyc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycBackendApplication.class, args);
+	}
+
+}
