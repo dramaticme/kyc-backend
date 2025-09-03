a015ab77f2a5d8227a28b50462cb59bf654e5c39
Sashi Sonkusare <sonkusaresakshibhojraj@gmail.com>
2025-09-03 19:00:50 +0530

    Post,Get Request Working

--- src/main/java/com/kycapp/kyc_backend/controller/UserKycController.java
+++ src/main/java/com/kycapp/kyc_backend/controller/UserKycController.java
@@ -20,6 +20,19 @@ public class UserKycController {
     System.out.println("Received KYC: " + userKyc);
     return userKycRepository.save(userKyc);
 }
+    // Update KYC by ID
+@PutMapping("/{id}")
+public UserKyc updateKyc(@PathVariable String id, @RequestBody UserKyc updatedKyc) {
+    return userKycRepository.findById(id).map(existingKyc -> {
+        existingKyc.setName(updatedKyc.getName());
+        existingKyc.setAadhaarNumber(updatedKyc.getAadhaarNumber());
+        existingKyc.setPanNumber(updatedKyc.getPanNumber());
+        existingKyc.setPhone(updatedKyc.getPhone());
+        existingKyc.setEmail(updatedKyc.getEmail());
+        return userKycRepository.save(existingKyc);
+    }).orElse(null);
+}
+
 
     // Get all KYC records
     @GetMapping
